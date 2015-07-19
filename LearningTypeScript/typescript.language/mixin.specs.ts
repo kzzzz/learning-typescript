6973cb2f79c2a14f9bdd74a743c4c679ccabe21b
KZ <zhiping72@gmail.com>
2015-07-19 16:27:25 +0200

    Mixin unit tests added

---
+++ LearningTypeScript/typescript.language/mixin.specs.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="../scripts/typings/jasmine/jasmine.d.ts" />
+/// <reference path="mixin.ts" />
+
+describe("Mixin", () => {
+
+    it("walk", () => {
+        var jantje = new Person("Jantje");
+
+        expect(jantje.walk()).toBe("Jantje is walking.");
+    });
+
+    it("talk", () => {
+        var jantje = new Person("Jantje");
+
+        expect(jantje.talk()).toBe("Jantje is talking.");
+    });
+})
